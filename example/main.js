d8a342d20a7c4b75751fa6aed88fead74e62302a
姜明辉 <jiangmh@bibibi.net>
2023-02-07 00:45:03 +0800

    feat(*):create basic function

--- example/main.js
+++ example/main.js
@@ -1,5 +1,3 @@
 import { foo } from './src/foo.js'
-import bar from './src/bar.js'
 foo()
-bar()
 console.log('main.js')
--- index.js
+++ index.js
@@ -1,15 +1,69 @@
 import fs from 'fs'
 import parser from '@babel/parser'
-function creatAssets() {
+import traverse from "@babel/traverse";
+import path from 'path';
+import ejs from 'ejs'
+import * as babel from 'babel-core';
+// console.log(traverse)
+let id = 0
+function createAssets(filePath) {
     // 获取文件相互依赖关系
     // ast 抽象语法树
-    const source = fs.readFileSync('./example/main.js', {
+    const source = fs.readFileSync(filePath, {
         encoding:'utf-8'
     })
     const ast = parser.parse(source, {
         sourceType: "module",
     })
-    console.log(ast)
-    return {}
+    const deps = []
+    traverse.default(ast, {
+        ImportDeclaration({ node }) {
+            // console.log(node.source.value)
+            deps.push(node.source.value)
+        }
+      });
+      const { code } = babel.transformFromAst(ast, null, {
+        presets:["env"]
+      });
+    console.log(code)
+    return {
+        filePath,
+        code,
+        deps,
+        id: id++,
+        mapping:{},
+    }
 }
-creatAssets()
+
+function createGraph() {
+    const mainAssets = createAssets('./example/main.js')
+    let queue = [mainAssets]
+    for(const asset of queue) {
+        asset.deps.forEach(relativePath => { 
+            const child = createAssets(path.resolve('example', relativePath))
+            asset.mapping[relativePath] = child.id
+            queue.push(child)
+        });
+    }
+    return queue
+}
+const graph = createGraph()
+// console.log(graph)
+function build(graph) {
+    const template = fs.readFileSync('./bundle.ejs', {
+        encoding:'utf-8'
+    })
+    const data = graph.map(asset => {
+        const { code, id, mapping } = asset
+        return {
+            code,
+            id,
+            mapping,
+        }
+    })
+    console.log(data)
+    
+    const code = ejs.render(template, {data})
+    fs.writeFileSync('./dist/bundle.js', code)
+}
+build(graph)
